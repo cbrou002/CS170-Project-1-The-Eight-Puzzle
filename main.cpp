b00d6b6f67ee188073423c00a1161f5e5e6f05fe
cbrou002 <129991644+cbrou002@users.noreply.github.com>
2025-02-03 23:56:41 +0000

    worked on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
 
+using namespace std;
+
+//Function output puzzle
+
+//node structure
+
+//Function uniform cost search (same for all 3 ptypes of searches)
+
+int main(){
+    //input 1 for default puzzle and 2 for custom puzzle
+    char puzzle_mode;
+    cout << "Welcome to my 8-puzzle solver: Type '1' to use a default 8-puzzle, or type '2' to make a custom 8-puzzle \n";
+    cin >> puzzle_mode;
+    if(puzzle_mode == '1'){ // default puzzle
+        
+    }
+    else if(puzzle_mode == '2'){ // custom puzzle
+        
+    }
+
+    //if input 1
+        // cout << select algorithm 1. Uniform Cost Search, 2. Misplaced Tile Heuristic, 3. Manhattan Distance Heuristic
+        //call functions respectively
+    //if input 2
+        // cout << select algorithm 1. Uniform Cost Search, 2. Misplaced Tile Heuristic, 3. Manhattan Distance Heuristic
+        //read input for puzzle by rows
+    return 0;
+}
