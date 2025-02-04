2d378814198ea9da93038b00b378ed18827a8a34
cbrou002 <129991644+cbrou002@users.noreply.github.com>
2025-02-04 03:43:29 +0000

    added node structure, and worked on input in main function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <queue>
 
@@ -22,30 +23,47 @@ vector<vector<int>> hard_puzzle = {{ 8, 7, 1 },
 vector<vector<int>> impossible_puzzle = {{ 8, 6, 7 },
                                          { 2, 5, 4 },
                                          { 3, 0, 1 }};
-//Function output puzzle
 
-//node structure
+struct Node{ //node structure
+    vector<vector<int>> state; //current state of the puzzle
+    vector<vector<int>>* parent;//previous state of the puzzle
+    Node(const vector<vector<int>>& s, Node* p = nullptr){//Node Constructor
+        state = s;
+        parent = p;
+    }
+};
 
-//Function uniform cost search (same for all 3 ptypes of searches)
+void printPuzzle(vector<vector<int>> puzzle){//Prints the puzzle
+    for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                cout << puzzle[i][j] << " ";
+            }
+            cout << endl;
+        }
+}
+
+//Function uniform cost search (same for all 3 types of searches)
 
 int main(){
     //input 1 for default puzzle and 2 for custom puzzle
-    char puzzle_mode = 0;
-    int difficulty = -1;
-    vector<vector<int>> user_puzzle;
-    cout << "Welcome to my 8-puzzle solver: Type '1' to use a default 8-puzzle,
-             or type '2' to make a custom 8-puzzle. \n";
-    while(puzzle_mode != '1' && puzzle_mode != '2') //checks for valid input
+    int puzzle_mode = 0;
+    int temp;
+    vector<vector<int>> user_puzzle(3, vector<int>(3));
+
+    cout << "Welcome to my 8-puzzle solver: Type (1) to use a default 8-puzzle, or type (2) to make a custom 8-puzzle. \n";
+    while(puzzle_mode != 1 && puzzle_mode != 2) //checks for valid input
     {
         cin >> puzzle_mode;
     }
-    if(puzzle_mode == '1'){ // sets user puzzle to default puzzle
-        cout << "You wish to use a default puzzle. 
-                Please enter a desired difficulty on a scale from 0 to 5.\n";
-        while (difficulty < 0 || difficulty > 5)
-        cin >> difficulty;
+    int difficulty = -1;
+    if(puzzle_mode == 1){ /////////////////////////////////////////////// CREATE DEFAULT PUZZLE
+        cout << "You wish to use a default puzzle. "; 
+        while (difficulty < 0 || difficulty > 5){//checks for valid input
+            cout << "Please enter a desired difficulty on a scale from 0 to 5.\n";
+            cin >> difficulty;
+        }
         if (difficulty == 0){
-            cout << "Trival difficulty selected. \n";
+            cout << "Trivial difficulty selected. \n";
             user_puzzle = trivial_puzzle;
         }
         if (difficulty == 1){
@@ -68,21 +86,44 @@ int main(){
             cout << "Trival difficulty selected.\n";
             user_puzzle = impossible_puzzle;
         }
+        //printPuzzle(user_puzzle);
     }
-    else if(puzzle_mode == '2'){ // sets user puzzle to custom puzzle
-        //prompt to get first row
-        //get input for first row
-        //prompt to get second row
-        //get input for second row
-        //prompt to get third row
-        //get input for third row
+    
+    else if(puzzle_mode == 2){ /////////////////////////////////////////// CREATE CUSTOM PUZZLE
+        string row_input;
+        cout << "Enter your puzzle, using a zero to represent the blank. Please only enter valid 8-puzzles.\n";
+        cout << "Enter the first row (separated by spaces and use 0 to represent the blank space), then hit enter\n";
+        for(int i = 0; i < 3; i++){
+            cin >> temp;
+            user_puzzle[0][i] = temp;
+        }
+        cout << "Enter the second row (separated by spaces and use 0 to represent the blank space), then hit enter\n";
+        for(int i = 0; i < 3; i++){
+            cin >> temp;
+            user_puzzle[1][i] = temp;
+        }
+        cout << "Enter the third row (separated by spaces and use 0 to represent the blank space), then hit enter\n";
+        for(int i = 0; i < 3; i++){
+            cin >> temp;
+            user_puzzle[2][i] = temp;
+        }
+        cout << endl;
+        //printPuzzle(user_puzzle);
     }
+    ///////////////////////////////////////////////////PICKING ALGORITHM
+    temp = -1;
+    while(temp != 1 && temp != 2 && temp != 3){//checks for valid input
+        cout << "Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, or (3) the Manhattan Distance Heuristic.\n";
+        cin >> temp;
+    }
+    if(temp == 1){ //Uniform Cost Search
 
-    //if input 1
-        // cout << select algorithm 1. Uniform Cost Search, 2. Misplaced Tile Heuristic, 3. Manhattan Distance Heuristic
-        //call functions respectively
-    //if input 2
-        // cout << select algorithm 1. Uniform Cost Search, 2. Misplaced Tile Heuristic, 3. Manhattan Distance Heuristic
-        //read input for puzzle by rows
+    }
+    else if(temp == 2){ //Misplaced Tile Heuristic
+        
+    }
+    else if(temp == 3){ //Manhattan Distance Heuristic
+
+    }
     return 0;
 }
